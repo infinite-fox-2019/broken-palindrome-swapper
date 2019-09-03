a621cc68139be4e9e1ea23de149c9cda54718744
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-03 10:47:50 +0700

    palindromeSwap Done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -8,19 +8,28 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
+    if (str.split('').reverse().join('') === str) {
+      return true};
+      debugger;
+      for (var j = 0; j < str.length; j++) {
+        debugger;
+        if (i === j) {
+          newStr += str[j+1];
+          debugger;
+          newStr += str[j];
+          j++;
+        } else {
+          newStr += str[j];
+        }
       }
-    }
-    if (isPalindrome(newStr)) return true;
+      debugger;
+      if (isPalindrome(newStr)) {
+        return true;
+      } else {
+        return false;
+      }
   }
-  return false;
-}
+};
 
 /**
  * @function isPalindrome
@@ -29,8 +38,12 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  debugger;
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
